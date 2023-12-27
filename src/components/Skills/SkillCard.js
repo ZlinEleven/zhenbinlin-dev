42b69d6415a47d7b9f2105bba7f93f3d6b0ee764
Zhenbin Lin <zhenbin2004@gmail.com>
2023-12-27 10:56:25 -0500

    Added responsive design for skills section

--- src/components/Skills/SkillCard.js
+++ src/components/Skills/SkillCard.js
@@ -4,7 +4,7 @@ const SkillCard = ({ title, icon, isActive, onClick}) => {
   return (
     <div onClick={onClick}>
         <div className={`flex flex-col items-center p-5 md:p-10 m-[0.675rem] md:m-[20px] cursor-pointer
-        w-[105px] md:w-[210px] md:h-[180px]
+        w-[105px] md:w-[210px] h-[90px] md:h-[180px]
         ${isActive ? "bg-gradient-to-r from-[#aa94fe] via-putple-500 to-[#7c5fe6]" : "bg-[#151146]"}
         hover:bg-gradient-to-r from-[#aa94fe] via-putple-500 to-[#7c5fe6] 
         rounded-[0.65rem] border-solid border-[3px] border-[#704cf2] bg-[#151146] duration-1000 text-white`} /*className={`flex flex-col items-center w-1/5 cursor-pointer rounded-[0.65rem] border-[3px] border-solid border-[#704cf2] p-10 ${isActive ? "bg-gradient-to-r from-[#aa94fe] via-putple-500 to-[#7c5fe6]" : "bg-[#151146]"} hover:bg-gradient-to-r from-[#aa94fe] via-putple-500 to-[#7c5fe6] duration-1000 text-white`}*/>
--- src/components/Skills/SkillsInfoCard.js
+++ src/components/Skills/SkillsInfoCard.js
@@ -4,26 +4,25 @@ import React from 'react'
 
 const SkillsInfoCard = ({ skills }) => {
     return (
-        <div className='grid grid-cols-3 gap-10 mt-[1.25rem]'>
+        <div className='grid grid-cols-3 md:gap-10 mt-[1.25rem]'>
             {
-                skills.map((skill, index) => {
+                skills.map((skill) => {
                     return (
-                        <div className='group'>
+                        <div className='group mt-3 md:mt-0 mb-6 md:mb-0'>
                             <div className="flex flex-col relative items-center justify-center 
                         z-10 group-hover:translate-y-[-35px]
-                        w-[105px] md:w-[210px] h-[90px] md:h-[180px]
-                        rounded-[0.65rem] border-[3px] border-solid border-[#704cf2] bg-white text-gray-600 duration-500"
-                        /*'flex flex-col justify-center items-center py-7  md:mx-[-40px] w-[140px] md:w-[220px] rounded-[0.65rem] border-[3px] border-solid border-[#704cf2] bg-gradient-to-r from-[#aa94fe] via-putple-500 to-[#7c5fe6] text-white'*/>
+                        w-[105px] md:w-[160px] lg:w-[210px] h-[90px] md:h-[135px] lg:h-[180px] mx-auto
+                        rounded-[0.65rem] border-[3px] border-solid border-[#704cf2] bg-white text-gray-600 duration-500">
                                 <img src={skill.icon} alt="react" className='w-8 md:w-16' />
                                 <p className='text-[0.65rem] md:text-[1.3rem] font-[500]'>{skill.name}</p>
                             </div>
                             <div className='flex items-end py-4
-                            h-[120px] w-[105px] md:w-[210px] mx-auto mt-[-120px] group-hover:translate-y-[30px]
+                            w-[105px] md:w-[160px] lg:w-[210px] h-[60px] md:h-[120px] mx-auto mt-[-60px] md:mt-[-120px] group-hover:translate-y-[15px] md:group-hover:translate-y-[30px]
                             rounded-bl-[0.65rem] rounded-br-[0.65rem] bg-[#151146] duration-500'>
                                 <div className='flex flex-col w-full items-center'>
                                     <p className='text-[0.65rem] md:text-[1.3rem] font-[500] text-[#dd8cfa] mb-2'>{skill.percentage}%</p>
-                                    <div className='w-[90%] mx-auto bg-gray-200 rounded-full h-2.5 dark:bg-gray-700'>
-                                        <div className="bg-[#7c5fe6] h-2.5 rounded-full duration-500" style={{ width: `${skill.percentage}%` }} />
+                                    <div className='w-[90%] mx-auto bg-gray-200 rounded-full h-[0.325rem] md:h-[0.65rem]'>
+                                        <div className="bg-[#7c5fe6] h-[0.325rem] md:h-[0.65rem] rounded-full" style={{ width: `${skill.percentage}%` }} />
                                     </div>
                                 </div>
                             </div>
