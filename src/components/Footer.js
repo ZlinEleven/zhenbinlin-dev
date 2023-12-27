8a1d96ba84a8bc5358988e7cf966ab4cc2bca58e
Zhenbin Lin <zhenbin2004@gmail.com>
2023-12-26 20:56:00 -0500

    Added responsive design for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,11 +11,11 @@ const Footer = () => {
                 <h1 className='text-3xl py-4'>Zhenbin Lin</h1>
                 <p>CS @ Stony Brook University</p>
             </div>
-            <div className='grid grid-cols-2 gap-6'>
+            <div className='grid grid-cols-2 gap-6 lg:flex'>
                 <FooterCards text="Instagram" icon={<FontAwesomeIcon icon={faInstagram} color='#fae1b2' size='lg' className='py-2 mt-[-10px]' />} link='https://www.instagram.com/zlin_eleven/'/>
                 <FooterCards text="LinkedIn" icon={<FontAwesomeIcon icon={faLinkedin} color='#fae1b2' size='lg' className='py-2 mt-[-10px]' />} link='https://www.linkedin.com/in/zhenbin-lin'/>
                 <FooterCards text="Github" icon={<FontAwesomeIcon icon={faGithub} color='#fae1b2' size='lg' className='py-2 mt-[-10px]' />} link='https://github.com/ZlinEleven'/>
-                <FooterCards text="Discord" icon={<ImProfile color='#fae1b2' size='38' className='py-2 mt-[-10px]' />} link='https://drive.google.com/file/d/1JbtAwQlNt0Xo2QTBeJjw2I7yPyZuMwVt/view?usp=sharing'/>
+                <FooterCards text="Resume" icon={<ImProfile color='#fae1b2' size='38' className='py-2 mt-[-10px]' />} link='https://drive.google.com/file/d/1JbtAwQlNt0Xo2QTBeJjw2I7yPyZuMwVt/view?usp=sharing'/>
             </div>
         </div>
     )
--- src/components/FooterCards.js
+++ src/components/FooterCards.js
@@ -3,9 +3,9 @@ import React from 'react'
 const FooterCards = ({ icon, text, link }) => {
     return (
         <a href={link}>
-            <div className='group  w-20 h-20 '>
-                <div className='flex bg-[#2d415e] w-full h-full rounded-br-xl group-hover:translate-x-[3.75px] group-hover:translate-y-[3.75px] duration-500'></div>
-                <div className='flex flex-col w-full h-full mt-[-80px] group-hover:translate-x-[-3.75px] group-hover:translate-y-[-3.75px] bg-[#0e2546] rounded-tl-xl rounded-br-xl justify-center items-center text-white duration-500'>
+            <div className='group w-20 h-20 lg:w-40 lg:h-40'>
+                <div className='flex bg-[#2d415e] w-full h-full rounded-br-xl group-hover:translate-x-[4%] group-hover:translate-y-[4%] duration-500'></div>
+                <div className='flex z-10 flex-col w-full h-full mt-[-80px] lg:mt-[-160px] group-hover:translate-x-[-4%] group-hover:translate-y-[-4%] bg-[#0e2546] rounded-tl-xl rounded-br-xl justify-center items-center text-white duration-500'>
                     {icon}
                     <h1 className='font-medium text-xs'>{text}</h1>
                 </div>
