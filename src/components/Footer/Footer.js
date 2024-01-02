37b9908a99cedacc58e98e8b46871352754e5f1d
Zhenbin Lin <zhenbin2004@gmail.com>
2024-01-01 22:26:36 -0500

    Improved responsive design for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,12 +6,12 @@ import { ImProfile } from "react-icons/im";
 
 const Footer = () => {
     return (
-        <div className='flex h-60 bg-[#0a192f] text-[#ccd6f6] justify-evenly items-center'>
-            <div className='flex flex-col p-8 items-center'>
-                <h1 className='text-3xl py-4'>Zhenbin Lin</h1>
+        <div className='flex flex-col md:flex-row h-80 md:h-60 bg-[#0a192f] text-[#ccd6f6] justify-evenly items-center'>
+            <div className='flex flex-col px-2 text-center'>
+                <h1 className='text-3xl pb-4'>Zhenbin Lin</h1>
                 <p>CS @ Stony Brook University</p>
             </div>
-            <div className='grid grid-cols-2 gap-6 lg:flex'>
+            <div className='flex gap-4'>
                 <FooterCards text="Instagram" icon={<FontAwesomeIcon icon={faInstagram} color='#fae1b2' size='lg' className='py-2 mt-[-10px]' />} link='https://www.instagram.com/zlin_eleven/'/>
                 <FooterCards text="LinkedIn" icon={<FontAwesomeIcon icon={faLinkedin} color='#fae1b2' size='lg' className='py-2 mt-[-10px]' />} link='https://www.linkedin.com/in/zhenbin-lin'/>
                 <FooterCards text="Github" icon={<FontAwesomeIcon icon={faGithub} color='#fae1b2' size='lg' className='py-2 mt-[-10px]' />} link='https://github.com/ZlinEleven'/>
